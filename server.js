4a43fea91595d0f624f86b0f7cbbdd3fe7788aa1
Mitchell Porter <mitchellporter@gmail.com>
2017-04-25 12:54:13 -0700

    postgres knex connection

--- server.js
+++ server.js
@@ -3,11 +3,34 @@
 const Promise = require('bluebird');
 const bodyParser = require('body-parser');
 const morgan = require('morgan')('dev');
+const chalk = require('chalk');
 
-const api = require('./app/api/api');
+var pg = require('knex')({
+  client: 'pg',
+  connection: 'postgres://localhost:5432/express-knex',
+  searchPath: 'knex,public',
+  debug: true,
+  pool: {
+    afterCreate: function (conn, done) {
+        console.log('afterCreate called');
+      // in this example we use pg driver's connection API
+      conn.query('SET timezone="UTC";', function (err) {
+        if (err) {
+          // first query failed, return error and don't try to make next query
+          console.log(chalk.red(`connection error: ${err}`));
+          done(err, conn);
+        } else {
+            console.log(chalk.green('connection successful'));
+            done(null, conn);
+        }
+      });
+    }
+  }
+});
+
+// const api = require('./app/api/api');
 const express = require('express');
 const app = express();
-
 
 // middleware
 app.use(morgan);
@@ -15,29 +38,27 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 // routes
-app.use('/api/' + config.api_version, api);
+// app.use('/api/' + config.api_version, api);
 
 // errors
 app.use((err, req, res, next) => {
-    console.log(err);
+    console.log(chalk.red(err));
     res.send(err);
 });
 
 // Server setup
-var server = app.listen(config.port, () => {
-    logger.silly('pid: ' + process.pid + ' listening on port:' + config.port);
+var server = app.listen(3000, () => {
+    console.log('pid: ' + process.pid + ' listening on port:' + 3000);
 });
 
 // Shutdown
 var gracefulShutdown = () => {
-    logger.silly('Received SIGTERM signal, shutting down express server');
+    console.log('Received SIGTERM signal, shutting down express server');
     server.close();
 }
 
 process.on('SIGTERM', gracefulShutdown);
 
 server.on('close', () => {
-    logger.silly('Express server closed.. about to cleanup connections');
-
-    mongoose.disconnect();
+    console.log('Express server closed.. about to cleanup connections');
 });
